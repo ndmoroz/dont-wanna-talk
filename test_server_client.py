f53c1f9e35b08da479b95e647e0bb745c883f80d
Nikolay <ndmoroz@gmail.com>
2017-09-26 01:25:39 +0300

    Add script to run both server and client from one
    May be used in future for integration tests

---
+++ test_server_client.py
@@ -0,0 +1,12 @@
+from sys import executable
+from os.path import dirname
+from subprocess import Popen
+
+# Define a command that starts new terminal
+new_window_command = "cmd.exe /c start".split()
+
+# Open new consoles, run server and client
+project_path = " " + dirname(__file__)
+echos = [executable, project_path, "/server.py"], \
+        [executable, project_path, "/client.py"]
+processes = [Popen(new_window_command + [echo]) for echo in echos]
